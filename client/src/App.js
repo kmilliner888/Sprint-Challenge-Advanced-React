6b19be69f8114bd1d25678794eb04dc0932d6dbe
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-03-27 07:25:02 -0400

    Set up App and Players component with search input

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Players from './components/Players';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    wwc: [],
+    entry: ""
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then(response => {
+        // console.log("response", response); 
+        this.setState({ wwc : response.data})
+      })
+      .catch(error => console.log("error", error)); 
+  };
+
+  handleChanges = e => {
+    this.setState({
+      entry: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup Players</h1>
+        <br></br>
+        <div className="search-form-container">
+          <input
+            type="text"
+            placeholder="Search for a player"
+            value={this.state.entry}
+            onChange={this.handleChanges}
+          />
+          <button onClick={this.searchPlayer}>Search for Player</button>
+        </div>
+        <div className="players-container">
+          {this.state.wwc.map(player => {
+            if (player.name === this.state.entry) {
+              return <div className="players-card" 
+              key={player.id}>
+              <h1>{player.name}</h1>
+              <h2>{player.country}</h2>
+              <p>Searches: {player.searches}</p>
+              </div>
+            } else {
+              return "";
+            }
+          })} 
+        </div>
+        <Players wwc={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
